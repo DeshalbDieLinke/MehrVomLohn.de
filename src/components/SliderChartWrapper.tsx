15d3e136f9559bcc9e4574f5d553a49b02aa7535
Julian <81575762+Brownymister@users.noreply.github.com>
2025-01-24 18:07:33 +0100

    add voting recomendation

--- src/components/SliderChartWrapper.tsx
+++ src/components/SliderChartWrapper.tsx
@@ -1,21 +1,88 @@
 import Slider from "../components/Slider.tsx";
 import SteuernChart from "../components/SteuernChart.tsx";
+import type { RechartsData } from "../components/SteuernChart.tsx";
 import { useState } from "react";
 import taxdata from "../data/taxdata.json";
 import { CallToActionWrapper, Status } from "./CallToActionWrapper.tsx";
 
+export interface TaxGroup {
+    type: string;
+    linke: number[];
+    gruene: number[];
+    spd: number[];
+    bsw: number[];
+    cdu: number[];
+    fdp: number[];
+    afd: number[];
+}
+
+export interface TaxData {
+    taxgroups: TaxGroup[];
+    colors: { [key: string]: string };
+}
+
+function GetRecomendations(data: RechartsData[]): { party: string; status: Status; entlastung: number } {
+    let max = { v: 0, p: "" };
+    data.forEach((entry: RechartsData) => {
+        if (entry.value > max.v) {
+            max = { v: entry.value, p: entry.name };
+        }
+    });
+    let status = Status.andere;
+    if (max.p == "linke") {
+        status = Status.linke;
+        if (max.v < 0) {
+            status = Status.linkenegativ;
+        }
+    }
+    return { party: max.p, status: status, entlastung: max.v };
+}
+
 export default function SliderChartWrapper() {
     let [userdata, setUserdata] = useState({ income: 25000, children: false });
-    let [outputdata, setOutputData] = useState({ party: "", status: Status.andere, entlastung: 0 });
+
+    let data = CalcGraphData(taxdata.taxgroups, taxdata.colors, userdata);
+
+    let recom = GetRecomendations(data);
 
     return (
         <>
-            {outputdata.party}
-            {outputdata.status}
-            {outputdata.entlastung}
             <Slider value={userdata} setValue={setUserdata} />
-            <SteuernChart setValue={setOutputData} userinput={userdata} data={taxdata} />
-            {outputdata.party != "" && <CallToActionWrapper output={outputdata} />}
+            <SteuernChart data={data} />
+            <CallToActionWrapper output={recom} />
         </>
     );
+}
+
+function UserIncomeIsInGroup(userincome: number, taxgroup: number): boolean {
+    return userincome <= taxgroup;
+}
+
+function isNumberString(value: string): boolean {
+    return !isNaN(Number(value));
+}
+
+function CalcGraphData(taxgroups: TaxGroup[], colors: any, userinput: any): RechartsData[] {
+    let data: RechartsData[] = [];
+    let found = false;
+    taxgroups.forEach((taxgroup: TaxGroup) => {
+        if (
+            // !props.userinput.children &&
+            isNumberString(taxgroup.type) &&
+            UserIncomeIsInGroup(userinput.income, parseInt(taxgroup.type, 10)) &&
+            !found
+        ) {
+            for (const [key, value] of Object.entries(taxgroup)) {
+                if (key !== "type") {
+                    data.push({ name: key, value: value[0] * (userinput.income / 100), color: colors[key] });
+                }
+            }
+            found = true;
+        }
+    });
+
+    data.forEach((entry) => {
+        entry.value = Math.round(entry.value);
+    });
+    return data;
 }
--- src/components/SteuernChart.tsx
+++ src/components/SteuernChart.tsx
@@ -1,91 +1,23 @@
 import React from "react";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell } from "recharts";
-import { Status } from "./CallToActionWrapper.tsx";
 
-function isNumberString(value: string): boolean {
-    return !isNaN(Number(value));
-}
-
-interface TaxGroup {
-    type: string;
-    cdu: [number, number];
-    linke: [number, number];
-    spd: [number, number];
-    gruene: [number, number];
-    bsw: [number, number];
-    fdp: [number, number];
-    afd: [number, number];
-}
-
-interface TaxData {
-    taxgroups: TaxGroup[];
-    colors: { [key: string]: string };
-}
-
-interface RechartsData {
+export interface RechartsData {
     name: string;
     value: number;
     color: string;
 }
 
-function UserIncomeIsInGroup(userincome: number, taxgroup: number): boolean {
-    return userincome <= taxgroup;
-}
-
-const SteuernChart = (props: {
-    setValue: (value: { party: string; status: Status; entlastung: number }) => void;
-    data: TaxData;
-    userinput: { income: number; children: boolean };
-}) => {
-    let data: RechartsData[] = [];
-    let found = false;
-    props.data.taxgroups.forEach((taxgroup) => {
-        if (
-            // !props.userinput.children &&
-            isNumberString(taxgroup.type) &&
-            UserIncomeIsInGroup(props.userinput.income, parseInt(taxgroup.type, 10)) &&
-            !found
-        ) {
-            for (const [key, value] of Object.entries(taxgroup)) {
-                if (key !== "type") {
-                    data.push({ name: key, value: value[0] * (props.userinput.income / 100), color: props.data.colors[key] });
-                }
-            }
-            found = true;
-        }
-    });
-
-    data.forEach((entry) => {
-        entry.value = Math.round(entry.value);
-    });
-
-    // find the heigest value
-    let max = { v: 0, p: "" };
-    data.forEach((entry) => {
-        if (entry.value > max.v) {
-            max = { v: entry.value, p: entry.name };
-        }
-    });
-    let status = Status.andere;
-    if (max.p == "linke") {
-        status = Status.linke;
-        if (max.v < 0) {
-            status = Status.linkenegativ;
-        }
-    }
-    props.setValue({ party: max.p, status: status, entlastung: max.v });
-    console.log("here");
-
+const SteuernChart = (props: { data: RechartsData[] }) => {
     return (
         <ResponsiveContainer width="100%" height={600}>
-            <BarChart data={data}>
+            <BarChart data={props.data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
                 <Bar isAnimationActive={false} dataKey="value">
-                    {data.map((entry, index) => (
+                    {props.data.map((entry, index) => (
                         <Cell key={`cell-${index}`} fill={entry.color} />
                     ))}
                 </Bar>
