c4c28e2a6373cda20e7c016b2947e353f56bfdab
Julian <81575762+Brownymister@users.noreply.github.com>
2025-01-26 18:57:14 +0100

    pimp chart

--- src/components/SteuernChart.tsx
+++ src/components/SteuernChart.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell, LabelList } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell, LabelList, Label } from "recharts";
 
 export interface RechartsData {
     name: string;
@@ -12,15 +12,18 @@ const SteuernChart = (props: { data: RechartsData[] }) => {
         <ResponsiveContainer width="100%" height={600}>
             <BarChart data={props.data}>
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
+                <XAxis dataKey="name" tick={false}>
+                    <Label value="Parteien" offset={0} position="insideBottom" />
+                </XAxis>
+                <YAxis>
+                    <Label value="Steuern in €" offset={0} position="insideLeft" angle={-90} />
+                </YAxis>
                 <Tooltip />
-                <Legend />
                 <Bar isAnimationActive={false} dataKey="value">
                     {props.data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={entry.color}  />
+                        <Cell key={`cell-${index}`} fill={entry.color} />
                     ))}
-                    <LabelList dataKey="name" position="outside" />
+                    <LabelList dataKey="name" position="inside" fill="#ffffff" angle={-90} fontSize={20} />
                 </Bar>
             </BarChart>
         </ResponsiveContainer>
