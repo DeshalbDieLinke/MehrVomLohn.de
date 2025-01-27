9a9e95fce04fc822221e97481f61c9dbd1db28b7
Julian <81575762+Brownymister@users.noreply.github.com>
2025-01-27 16:33:51 +0100

    add 2mio input limit

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,17 +14,20 @@ export default function InputComponent(props: {
             <div className="pt-8 px-8 flex flex-col gap-8 py-8">
                 <h1 className="text-3xl">Wie hoch ist dein Bruttoeinkommen im Jahr in Euro?</h1>
                 <p>Gib entweder deine Daten in Euro anonym ein oder nutze den Regler.</p>
-                <Input type="number" value={props.value.income} onChange={handleChange} />
+                <Input type="number" value={props.value.income} onChange={handleChange} max={2_000_000_0} />
                 <input type="range" min={0} max={130000} className="range" value={props.value.income} onChange={handleChange} />
                 <label>
-                    Bist du verheiratet und hast 2 Kinder? <input 
-                        type="checkbox" 
-                        name="children" 
+                    Bist du verheiratet und hast 2 Kinder?{" "}
+                    <input
+                        type="checkbox"
+                        name="children"
                         checked={props.value.children}
-                        onChange={(e) => props.setValue({ 
-                            ...props.value, 
-                            children: e.target.checked 
-                        })}
+                        onChange={(e) =>
+                            props.setValue({
+                                ...props.value,
+                                children: e.target.checked,
+                            })
+                        }
                     />
                 </label>
             </div>
