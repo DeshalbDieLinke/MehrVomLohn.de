bd8b7523c1568f4e683d70c57062f83ca06f1213
Julian <81575762+Brownymister@users.noreply.github.com>
2025-02-15 21:14:44 +0100

    fix data, add dropdown

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,49 +2,54 @@ import { Input } from "./ui/input.tsx";
 import { useState, type ChangeEvent } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
+function CleanIncomeData(income: number, children: string): number {
+    let value = income;
+    console.log(children);
+    if (children == "twochilden" || children == "paar") {
+        if (value <= 40000) {
+            value = 40000;
+        } else if (value <= 60000) {
+            value = 60000;
+        } else if (value <= 80000) {
+            value = 80000;
+        } else if (value <= 120000) {
+            value = 120000;
+        } else {
+            value = 180000;
+        }
+    } else if (children == "single") {
+        // round up the value into steps: 10000, 20000, 30000, 40000, 55000, 80000, 100000, 150000, 250000, 2000000
+        if (value <= 10000) {
+            value = 10000;
+        } else if (value <= 20000) {
+            value = 20000;
+        } else if (value <= 30000) {
+            value = 30000;
+        } else if (value <= 40000) {
+            value = 40000;
+        } else if (value <= 55000) {
+            value = 55000;
+        } else if (value <= 80000) {
+            value = 80000;
+        } else if (value <= 100000) {
+            value = 100000;
+        } else if (value <= 150000) {
+            value = 150000;
+        } else if (value <= 250000) {
+            value = 250000;
+        } else {
+            value = 2000000;
+        }
+    }
+    return value;
+}
+
 export default function InputComponent(props: {
     value: { income: number; children: string };
     setValue: (value: { income: number; children: string }) => void;
 }) {
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        // round up the value into steps: 40000 60000 80000 120000 >180000
-        var value = parseInt(event.target.value);
-        if (props.value.children == "twochilden" || props.value.children == "paar") {
-            if (value < 40000) {
-                value = 40000;
-            } else if (value < 60000) {
-                value = 60000;
-            } else if (value < 80000) {
-                value = 80000;
-            } else if (value < 120000) {
-                value = 120000;
-            } else {
-                value = 180000;
-            }
-        } else if (props.value.children == "single") {
-            // round up the value into steps: 10000, 20000, 30000, 40000, 55000, 80000, 100000, 150000, 250000, 2000000
-            if (value < 10000) {
-                value = 10000;
-            } else if (value < 20000) {
-                value = 20000;
-            } else if (value < 30000) {
-                value = 30000;
-            } else if (value < 40000) {
-                value = 40000;
-            } else if (value < 55000) {
-                value = 55000;
-            } else if (value < 80000) {
-                value = 80000;
-            } else if (value < 100000) {
-                value = 100000;
-            } else if (value < 150000) {
-                value = 150000;
-            } else if (value < 250000) {
-                value = 250000;
-            } else {
-                value = 2000000;
-            }
-        }
+        const value = CleanIncomeData(parseInt(event.target.value), props.value.children);
         props.setValue({ ...props.value, income: value });
     };
 
@@ -53,11 +58,48 @@ export default function InputComponent(props: {
             <div className="flex flex-col gap-8 py-8">
                 <div>
                     <h1 className="text-3xl ">Wie hoch ist dein Bruttoeinkommen im Jahr in Euro?</h1>
-                    <p className="text-xl">Gib deine Daten in Euro anonym im Nummernfeld ein oder nutze den Regler.</p>
+                    <p className="text-xl">Wähle deine Daten in Euro anonym im Menu ein oder nutze den Regler.</p>
                 </div>
                 <div className="flex flex-col sm:flex-row gap-4">
                     <div className="w-full">
-                        <Input type="number" value={props.value.income} onChange={handleChange} max={2_000_000_0} />
+                        <Select
+                            onValueChange={(e) => {
+                                props.setValue({
+                                    ...props.value,
+                                    income: parseInt(e),
+                                });
+                            }}
+                            value={CleanIncomeData(props.value.income, props.value.children).toString()}
+                        >
+                            <SelectTrigger>
+                                <SelectValue />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {props.value.children == "single" && (
+                                    <>
+                                        <SelectItem value="10000">0€-10000€</SelectItem>
+                                        <SelectItem value="20000">10001€-20000€</SelectItem>
+                                        <SelectItem value="30000">20001€-30000€</SelectItem>
+                                        <SelectItem value="40000">30001€-40000€</SelectItem>
+                                        <SelectItem value="55000">40001€-55000€</SelectItem>
+                                        <SelectItem value="80000">55001€-80000€</SelectItem>
+                                        <SelectItem value="100000">80001€-100000€</SelectItem>
+                                        <SelectItem value="150000">100001€-150000€</SelectItem>
+                                        <SelectItem value="250000">150001€-250000€</SelectItem>
+                                        <SelectItem value="2000000">250001€-2000000€</SelectItem>
+                                    </>
+                                )}
+                                {props.value.children != "single" && (
+                                    <>
+                                        <SelectItem value="40000">40000€</SelectItem>
+                                        <SelectItem value="60000">60000€</SelectItem>
+                                        <SelectItem value="80000">80000€</SelectItem>
+                                        <SelectItem value="120000">120000€</SelectItem>
+                                        <SelectItem value="180000">180000€</SelectItem>
+                                    </>
+                                )}
+                            </SelectContent>
+                        </Select>
                     </div>
                     <div className="w-full sm:w-[180px]">
                         <Select
